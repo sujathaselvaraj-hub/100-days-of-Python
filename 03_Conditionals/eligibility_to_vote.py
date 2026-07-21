ea0b482a21aa902d501aab6c4c67fd1d5ad02738
sujathaselvaraj-hub <sujathainreallife@gmail.com>
2026-07-21 06:13:30 +0530

    Eligibility to vote

---
+++ 03_Conditionals/eligibility_to_vote.py
@@ -0,0 +1,6 @@
+print("Check whether you are eligible to vote or not")
+age= int(input("Enter your age: "))
+if age>=18:
+      print("You are eligible to vote")
+else:
+      print("You are not eligible to vote")
