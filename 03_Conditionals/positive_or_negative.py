e080e726f0caba4d377b07b7cc7620e5048e7229
sujathaselvaraj-hub <sujathainreallife@gmail.com>
2026-07-21 06:01:21 +0530

    Positive or Negative number.

---
+++ 03_Conditionals/positive_or_negative.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a positive or a negative number: "))
+if number>0:
+    print(number,"-is positive")
+elif number<0:
+    print(number, "is negative")
+else:
+    print("The number is 0")
