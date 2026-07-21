1e9c17ace663ee814c62e30ea4520e1160891879
sujathaselvaraj-hub <sujathainreallife@gmail.com>
2026-07-21 05:57:59 +0530

    Simple Interest.

---
+++ 01_Basic/simple_interest.py
@@ -0,0 +1,7 @@
+p= int(input("Insert Principal:"))
+r= int(input("Insert Rate in Percentage:"))
+t=int(input("Insert Time in years:"))
+SI=(p*r*t)/100
+Total= p+SI
+print("Simple Interest",SI)
+print("Total Amount",Total)
