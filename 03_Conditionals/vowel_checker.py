a5ef048d0cec618f4883f26b383c6fdb7abde09b
sujathaselvaraj-hub <sujathainreallife@gmail.com>
2026-07-21 06:28:45 +0530

    Vowel checker.

---
+++ 03_Conditionals/vowel_checker.py
@@ -0,0 +1,5 @@
+letter= str(input("Enter a letter: "))
+if letter=='a' or letter== 'e' or letter== 'i' or letter== 'o' or letter=='u':
+    print(letter, "is a vowel")
+else:
+    print(letter, "is not a vowel")
